43a08d02796a0f543ee327221f7c4fc6da5dff7c
Gleb <lobzison@gmail.com>
2018-03-28 20:34:01 +0200

    now it looks good bot dont work

--- src/BotHandler.py
+++ src/BotHandler.py
@@ -9,7 +9,11 @@ class BotHandler:
         self.api_url = 'https://api.telegram.org/bot{}/'.format(token)
         self.language = {"from": "ru", "to": "pl"}
         self.translate_key = translate_key
+        self.offset = None
+        self.response_types = ['text']
+        self.commands = ['pshek']
 
+    # API part
     def send_message(self, chat, message):
         """Send message to the chat."""""
         method = 'sendMessage'
@@ -17,25 +21,17 @@ class BotHandler:
         response = requests.post(self.api_url + method, params)
         return response
 
-    def get_updates(self, offset=None, timeout=30):
+    def get_updates(self, timeout=30):
         """Gets json of messages"""
         method = 'getUpdates'
-        params = {'timeout': timeout, 'offset': offset}
+        params = {'timeout': timeout, 'offset': self.offset}
         response = requests.get(self.api_url + method, params)
         result_json = response.json()['result']
         return result_json
 
-    def get_last_update(self):
-        """Gets last update"""
-        get_result = self.get_updates()
+    # /API part
 
-        if len(get_result) > 0:
-            last_update = get_result[-1]
-        else:
-            last_update = []
-
-        return last_update
-
+    # setters
     def change_language(self):
         """Changes the language back and forth"""
         lng_to = self.language["to"]
@@ -43,34 +39,84 @@ class BotHandler:
         self.language["to"] = lng_from
         self.language["from"] = lng_to
 
-    def get_translation_obj(self):
-        """Creates translate object from key"""
+    def set_offset(self, offset):
+        """Setter for offset"""
+        self.offset = offset
+
+    #/ setters
+
+    # getters
+    def get_translation(self, text):
+        """Translates text"""
         translate = YandexTranslate(self.translate_key)
-        return translate
+        text = translate.translate(
+            text, '{}-{}'.format(self.language["from"], self.language["to"]))
+        return text["text"]
 
-    def handle_update(self, update):
-        """Handles incoming updates"""
-        if update != []:
-            update_id = update['update_id']
-            last_chat_id = update['message']['chat']['id']
+    def get_text(self, message):
+        """Gets text part of message"""
+        return message['text']
 
-            if 'message' not in update or 'text' not in update['message']:
-                self.send_message(
-                    last_chat_id, 'PSHEK PSHEK PSHEK NE PONIMAY PISHI TEKST')
-            else:
-                last_chat_text = update['message']['text']
-                if last_chat_text.lower() == "/pshek":
-                    self.change_language()
-                    l_message = self.language
-                    self.send_message(
-                        last_chat_id, 'NOW TRANSLATING %s' % l_message)
+    def get_msg_type(self, message):
+        """Returns type of message"""
+        for typ in self.response_types:
+            if typ in message:
+                return typ
+
+    def get_meta(self, message):
+        """Returns metadata of message"""
+        meta = []
+        chat_id = message['chat']['id']
+        meta.append(chat_id)
+        return meta
+
+    def get_response(self, text):
+        """Gets response for text message"""
+        response = self.get_translation(text)
+        return response
+
+    def get_command(self, text):
+        """Checks if text contains commans
+        If yes - returns first, if no - returns None"""
+        for command in self.commands:
+            e_command = '/' + command
+            if e_command in text:
+                return e_command
+
+    #/ getters
+
+    def strip_update(self, update):
+        """Retuns message part of update, 
+        sets offset to the update_id of message"""
+        self.set_offset(update['update_id'])
+        return update['message']
+
+    def execute_command(self, command, chat_id):
+        """Executes appropriate action for a command"""
+        if command == self.commands[0]:
+            self.change_language
+            response = "Now translating from {} to {}".format(
+                self.language["from"].upper(),
+                self.language["to"].upper())
+            self.send_message(chat_id, response)
+
+#   main part
+
+    def handle_updates(self, updates):
+        """Hanles the update set and responces accordingly"""
+        # print(updates)
+        if not updates:
+            pass
+        for update in updates[-10:]:
+            print(update)
+            message = self.strip_update(update)
+            typ = self.get_msg_type(message)
+            if typ in self.response_types:
+                meta = self.get_meta(message)
+                text = self.get_text(message)
+                command = self.get_command(text)
+                if command:
+                    self.execute_command(command, meta[0])
                 else:
-                    translate = self.get_translation_obj()
-                    l_message = translate.translate(
-                        last_chat_text, '%s-%s' % (self.language["from"], self.language["to"]))
-                    if l_message["code"] != 200 or l_message["text"] == []:
-                        l_answer = 'YA OBOSRALSA, SORYAN'
-
-                    else:
-                        l_answer = l_message["text"]
-                    self.send_message(last_chat_id, l_answer)
+                    response = self.get_response(text)
+                    self.send_message(meta[0], response)
